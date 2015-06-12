28a8cc25b6f0ce131a0e60f55ad27fa3436ded86
Luigi <bifulcoluigi@gmail.com>
2015-06-12 18:17:13 +0200

    activation controller

--- appify/appify-examples/src/main/java/it/appify/examples/emsmobile/controller/ActivationController.java
+++ appify/appify-examples/src/main/java/it/appify/examples/emsmobile/controller/ActivationController.java
@@ -1,14 +1,19 @@
 package it.appify.examples.emsmobile.controller;
 
 import it.appify.annotations.Controller;
-import it.appify.annotations.OnPageReady;
 import it.appify.annotations.ViewElement;
 import it.appify.annotations.ViewHandler;
 import it.appify.app.WebApp;
 import it.appify.examples.emsmobile.model.EmsMobileModel;
 
-import com.google.gwt.core.shared.GWT;
+import java.util.Date;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.RepeatingCommand;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.i18n.client.DateTimeFormat.PredefinedFormat;
 
 @Controller(page = "activationPage")
 public class ActivationController {
@@ -21,35 +26,38 @@ public class ActivationController {
 	@ViewElement("bottomBar")
 	private Element bottomBar;
 
+	@ViewElement("sendBtn")
+	private Element sendBtn;
+
 	public ActivationController(WebApp<EmsMobileModel> app) {
 		this.app = app;
 	}
 
-	@OnPageReady
-	public void onPageReady() {
-		String currentStyle = activationContent.getAttribute("style");
-
-		int activationHeight = activationContent.getOffsetHeight();
-		int bottomHeight = bottomBar.getOffsetHeight();
-
-		// GWT.log("activationPage activationContent: " + activationContent.getId());
-		GWT.log("activationPage activationHeight: " + activationHeight);
-		GWT.log("activationPage bottomHeight: " + bottomHeight);
-		String[] props = currentStyle.split(";");
-		currentStyle = "";
-
-		for (String p : props) {
-			GWT.log("activationPage currentStyle: " + p);
-			if (p.contains("height")) {
-				currentStyle += "height: " + (activationHeight - bottomHeight) + "px; ";
-			} else {
-				currentStyle += p + "; ";
-			}
-			//
-		}
-		//activationContent.setAttribute("style", currentStyle);
-		//
-	}
+	// @OnPageReady
+	// public void onPageReady() {
+	// String currentStyle = activationContent.getAttribute("style");
+	//
+	// int activationHeight = activationContent.getOffsetHeight();
+	// int bottomHeight = bottomBar.getOffsetHeight();
+	//
+	// // GWT.log("activationPage activationContent: " + activationContent.getId());
+	// GWT.log("activationPage activationHeight: " + activationHeight);
+	// GWT.log("activationPage bottomHeight: " + bottomHeight);
+	// String[] props = currentStyle.split(";");
+	// currentStyle = "";
+	//
+	// for (String p : props) {
+	// GWT.log("activationPage currentStyle: " + p);
+	// if (p.contains("height")) {
+	// currentStyle += "height: " + (activationHeight - bottomHeight) + "px; ";
+	// } else {
+	// currentStyle += p + "; ";
+	// }
+	// //
+	// }
+	// activationContent.setAttribute("style", currentStyle);
+	//
+	// }
 
 	@ViewHandler(eventType = "click", viewId = "confirmModalBtn")
 	public void onActivatioConfirm() {
@@ -79,6 +87,68 @@ public class ActivationController {
 		disablaAll();
 		enableSection("NoteData");
 
+	}
+
+	@ViewHandler(eventType = "click", viewId = "sendBtn")
+	public void send() {
+		GWT.log("send....");
+		nextStage("sendLabel");
+	}
+	
+	@ViewHandler(eventType = "click", viewId = "placeArrivalBtn")
+	public void placeArrival() {
+		GWT.log("departure....");
+		nextStage("placeArrivalLabel");
+	}
+	
+	@ViewHandler(eventType = "click", viewId = "departureBtn")
+	public void departure() {
+		GWT.log("departure....");
+		nextStage("departureLabel");
+	}
+	
+	@ViewHandler(eventType = "click", viewId = "hospitalArrivalBtn")
+	public void hospitalArrival() {
+		GWT.log("hospitalArrival....");
+		nextStage("hospitalArrivalLabel");
+	}
+	
+	@ViewHandler(eventType = "click", viewId = "closureBtn")
+	public void clousure() {
+		GWT.log("clousure....");
+		closure();
+		
+	}
+	
+	protected void closure() {
+		app.getCurrentPage().setElementText("closureLabel", "Invio in corso...");
+		final Date d = new Date();
+		Scheduler.get().scheduleFixedDelay(new RepeatingCommand() {
+
+			@Override
+			public boolean execute() {
+				String formattedTime = DateTimeFormat.getFormat(PredefinedFormat.TIME_MEDIUM).format(d);
+				app.getCurrentPage().setElementText("closureLabel", formattedTime);
+				app.back();
+				return false;
+			}
+		}, 2500);
+		
+	}
+
+	protected void nextStage(final String viewId) {
+		app.getCurrentPage().setElementText(viewId, "Invio in corso...");
+		final Date d = new Date();
+		Scheduler.get().scheduleFixedDelay(new RepeatingCommand() {
+
+			@Override
+			public boolean execute() {
+				String formattedTime = DateTimeFormat.getFormat(PredefinedFormat.TIME_MEDIUM).format(d);
+				app.getCurrentPage().setElementText(viewId, formattedTime);
+				//TODO: disable element btn
+				return false;
+			}
+		}, 2500);
 	}
 
 	protected void showFooterInfo() {
@@ -126,6 +196,14 @@ public class ActivationController {
 
 	public void setBottomBar(Element bottomBar) {
 		this.bottomBar = bottomBar;
+	}
+
+	public Element getSendBtn() {
+		return sendBtn;
+	}
+
+	public void setSendBtn(Element sendBtn) {
+		this.sendBtn = sendBtn;
 	}
 
 }
