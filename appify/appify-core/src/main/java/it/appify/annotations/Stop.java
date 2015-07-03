c17d3f8865a58410dbc4bf06b1a762264449333d
Bifulco Luigi <Bifulco Luigi@192.168.90.236>
2015-07-03 17:08:38 +0200

    introducing service stop annotation

---
+++ appify/appify-core/src/main/java/it/appify/annotations/Stop.java
@@ -0,0 +1,36 @@
+package it.appify.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+ * Appify - a tiny frontend framework to build complex mobile apps.
+ * 
+ * Copyright (C) 2015 Luigi Bifulco Appify is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Method of a Service class annotated with {@link Stop} annotation will be
+ * invoked when your app starts. A service class MUST be annotated with the
+ * {@link Service} annotation.
+ * 
+ * @author Luigi
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Stop {
+
+}
