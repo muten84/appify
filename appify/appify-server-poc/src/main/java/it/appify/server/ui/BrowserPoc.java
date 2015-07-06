5bed2aa4a05ad3430ca36477bc309d8be0611723
Bifulco Luigi <Bifulco Luigi@192.168.90.236>
2015-07-06 17:59:30 +0200

    introducing java browser support webkit based

---
+++ appify/appify-server-poc/src/main/java/it/appify/server/ui/BrowserPoc.java
@@ -0,0 +1,26 @@
+package it.appify.server.ui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.swing.BrowserView;
+
+public class BrowserPoc {
+
+	public static void main(String[] args) {
+		Browser browser = new Browser();
+        BrowserView browserView = new BrowserView(browser);
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.add(browserView, BorderLayout.CENTER);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        browser.loadURL("http://www.google.com");
+	}
+}
